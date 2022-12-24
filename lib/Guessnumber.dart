c83d27072885cf32eb1004f26eb2caf592dfe91c
630710130 <120084203+630710130@users.noreply.github.com>
2022-12-24 13:40:16 +0700

    Add files via upload

--- lib/Guessnumber.dart
+++ lib/Guessnumber.dart
@@ -1,48 +1,53 @@
 import 'dart:io';
-import 'dart:math';
-
+import 'package:guessnumber/Random.dart';
 
 void main() {
-  var r = Random();
-  int answer = r.nextInt(100) + 1;
-  var iscorrect = false;
-  int total=1;
-  int? guess;
-  stdout.writeln('          ║ Guess The Number ║          ');
-  do
+  var maxrandom = 100;
+  var GameSum = 0;
+  var Score = <int>[];
+  var difficulty;
+  var playAgain;
+  print("║         GUESS THE NUMBER GAME        ║");
+  do {
+    var game = Game(maxrandom);
+    GuessResult? guessresult;
+    do {
+      stdout.write("Guess number :");
+      String? input = stdin.readLineSync();
+      var guess = int.tryParse(input!);
+      if (guess == null)
+      {
+        continue;
+      }
+      guessresult = game.checkAns(guess)!;
+      if (guessresult == GuessResult.correct)
+      {
+        print('$guess is Correct ');
+      } else if (guessresult == GuessResult.tooHigh)
+      {
+        print('$guess is too High');
+
+      } else if (guessresult == GuessResult.tooLow)
+      {
+        print('$guess is too Low');
+
+      }
+    } while (guessresult != GuessResult.correct);
+    ;
+    do {
+      stdout.write("Play again?(Y,N):");
+      String input = stdin.readLineSync()!.toUpperCase();
+      if (input == 'Y' || input == 'N')
+      {
+        playAgain = input;
+      }
+    } while (playAgain == null);
+    GameSum++;
+    Score.add(game.totalGuess);
+  } while (playAgain == 'Y');
+  print('you play $GameSum game ');
+  for (var i = 0; i < Score.length; i++)
   {
-
-    stdout.write(" Guess the number betweeen 1 and 100 : ");
-    String? input = stdin.readLineSync();
-    guess =int.tryParse(input!);
-    if (input == null)
-    {
-      continue;
-    }
-    if (guess == null)
-    {
-
-      continue;
-    }
-    iscorrect = doGuess(guess,answer,total)!;
-    total++;
-  } while(!iscorrect);
-
-}
-bool? doGuess(int guess, int answer, int total) {
-  if (guess == answer)
-  {
-    print('      $answer is Correct , total Guess $total      ');
-    return true;
-  }
-  else if (guess > answer) {
-    print('              $guess is Too high!                ');
-
-    return false;
-  }
-  else if (guess < answer) {
-    print('              $guess is Too Low!                 ');
-
-    return false;
+    print('🎅game #${i + 1}:guess ${Score[i]} time.');
   }
 }
---
+++ lib/Random.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+enum GuessResult
+{
+  correct,tooHigh,tooLow;
+}
+class Game
+{
+  final int answer;
+
+  int _totalGuess = 0;
+
+  //constructor
+  Game(int? maxRandom) :
+        answer = Random().nextInt(maxRandom!) + 1
+  {
+    //print('answer is $answer');
+  }
+  GuessResult? checkAns(int guess)
+  {
+    _totalGuess++;
+    if (guess == answer)
+    {
+      return GuessResult.correct;
+    }
+    if (guess > answer)
+    {
+      return GuessResult.tooHigh;
+    }
+    if (guess < answer)
+    {
+      return GuessResult.tooLow;
+    }
+  }
+  int get totalGuess
+  {
+    return _totalGuess;
+  }
+}
